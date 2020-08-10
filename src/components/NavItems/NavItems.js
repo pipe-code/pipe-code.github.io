a8af4077e5fc0d8438f091140b409c21cb9c32e5
pipe-code <felipea.0420@gmail.com>
2020-08-10 08:23:39 -0500

    hamburger added

--- src/components/NavItems/NavItems.js
+++ src/components/NavItems/NavItems.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from './NavItems.module.scss';
 
 const NavItems = () => {
+
+    const [hamburger, setHamburger] = useState( false );
+
+    const toggleHamburgerHandler = () => {
+        setHamburger( prevState => { return !prevState } )
+    }
+
     return (
         <div className={styles.NavItems}>
-            <ul>
-                <li><NavLink to="/posts" activeClassName={styles.Active} exact>Posts</NavLink></li>
+            <ul className={hamburger ? styles.isOpen : null}>
+                <li><NavLink to="/posts" activeClassName={styles.Active} exact onClick={toggleHamburgerHandler}>Posts</NavLink></li>
             </ul>
+            <div className={hamburger ? [styles.Hamburger, styles.isActive].join(' ') : styles.Hamburger} onClick={toggleHamburgerHandler}>
+                <div className={styles.box}>
+                    <div className={styles.inner}></div>
+                </div>
+            </div>
         </div>
     )
 }
