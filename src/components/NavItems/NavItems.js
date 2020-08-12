4e3eaf5bc487581a97f9b38ba16a034d8005042f
pipe-code <felipea.0420@gmail.com>
2020-08-12 12:32:29 -0500

    codepen link added

--- src/components/NavItems/NavItems.js
+++ src/components/NavItems/NavItems.js
@@ -4,6 +4,7 @@ import Icon from '@iconify/react';
 import instagramIcon from '@iconify/icons-mdi/instagram';
 import githubIcon from '@iconify/icons-mdi/github';
 import codeJson from '@iconify/icons-mdi/code-json';
+import codepenIcon from '@iconify/icons-mdi/codepen';
 
 import styles from './NavItems.module.scss';
 
@@ -21,6 +22,7 @@ const NavItems = () => {
             <ul className={hamburger ? styles.isOpen : null}>
                 <li><NavLink to="/posts" activeClassName={styles.Active} exact onClick={toggleHamburgerHandler}><Icon icon={codeJson} /></NavLink></li>
                 <li><a href="https://instagram.com/pipe.view" target="_blank" rel="noopener noreferrer"><Icon icon={instagramIcon} /></a></li>
+                <li><a href="https://codepen.io/pipe-pen" target="_blank" rel="noopener noreferrer"><Icon icon={codepenIcon} /></a></li>
                 <li><a href="https://github.com/pipe-code" target="_blank" rel="noopener noreferrer"><Icon icon={githubIcon} /></a></li>
             </ul>
             <div className={hamburger ? [styles.Hamburger, styles.isActive].join(' ') : styles.Hamburger} onClick={toggleHamburgerHandler}>
