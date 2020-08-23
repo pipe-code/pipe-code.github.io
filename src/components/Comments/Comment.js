c17023f62007a844dbcf8f82c6dd9f3623f8a029
pipe-code <felipea.0420@gmail.com>
2020-08-23 10:21:37 -0500

    date added to comments

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -3,6 +3,12 @@ import React from 'react';
 import styles from './Comment.module.scss';
 
 const Comment = (props) => {
+
+    const getFormattedDate = (date) => {
+        let formattedDate = new Date(Date.parse(date));
+        return formattedDate.getDate() + '/' + (formattedDate.getMonth() + 1) + '/' + formattedDate.getFullYear() + '-' + formattedDate.getHours() + ':' + formattedDate.getMinutes();
+    }
+
     return (
         <div className={styles.Comment}>
             <div className={styles.Content}>
@@ -10,7 +16,7 @@ const Comment = (props) => {
                     <img src={props.commentData.Thumbnail} />
                 </div>
                 <div className={styles.Text}>
-                    <small>{props.commentData.Name}</small>
+                    <small>{props.commentData.Name} [{getFormattedDate(props.commentData.createdAt)}]</small>
                     <p>{props.commentData.Comment}</p>
                 </div>
             </div>
